6c720082cf97b34716abe7fa529d88ab125ebdc4
Ananya Sharma <ananyasharma@gmail.com>
2026-07-23 21:00:53 +0530

    Created Chat Agent!

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,3 +1,20 @@
+import { getModel } from "../config/llmModels"
+
 export const chatAgent = async (params) => {
-    
+    const llm = await getModel("chat");
+    const prompt = "You are DopeAI and a very intelligent AI assistances."
+    const response = await llm.invoke([
+        {
+            "role":"system",
+            "content":prompt
+        },
+        {
+            "role":"human",
+            "content":state.prompt
+        }
+    ])
+    return{
+        ...state,
+        aiResponse:response.content
+    }
 }
